ec0a67a8c1af69b10cc89581f213f5e190e2e89c
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2022-04-09 12:10:11 +0900

    Add some comments

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,24 @@ use sdl2::pixels::Color;
 use std::time::Duration;
 
 fn main() {
+    // Initialize SDL2 system
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
+    // Create new window
     let window = video_subsystem
         .window("Main Window", 1280, 720)
         .position_centered()
         .build()
         .unwrap();
 
+    // Fill the main window with white
     let mut canvas = window.into_canvas().build().unwrap();
     canvas.set_draw_color(Color::RGB(255, 255, 255));
     canvas.clear();
     canvas.present();
 
+    // Main loop until end request
     let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -33,6 +37,7 @@ fn main() {
 
         canvas.present();
 
+        // FPS limitation
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
